1d78b77d23435f71b355702a40d9f67a766dc62d
ryujihagiwara <dragon7103next@icloud.com>
2020-09-03 21:46:07 +0900

    customersコントローラーアクション追加

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,24 @@
 class Public::CustomersController < ApplicationController
+
+	def show
+	  @customer = Customer.find(current_user.id)
+	end
+
+	def edit
+	  @customer = Customer.find(current_user.id)
+	end
+
+	def update
+	  @customer = Customer.find(current_user.id)
+	  @customer.update(customer_params)
+	  redirect_to customers_my_page_path
+	end
+
+	def unsubscribe
+	end
+
+	def withdraw
+	　
+	end
+
 end
