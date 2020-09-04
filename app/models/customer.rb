3f8714c1c3bb448d08c7b432c5a9856ea94fe09a
vagrant <vagrant@localhost.localdomain>
2020-09-04 02:17:42 +0000

    admin_order

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,8 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum delete_flag: { withdraw: false, validity: true}
+  #退会ステータス
+  has_many :orders, dependent: :destroy
 end
