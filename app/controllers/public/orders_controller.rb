e5a2b190fda1082985fe8ca86bdc8b03a13f43bd
ryujihagiwara <dragon7103next@icloud.com>
2020-09-06 20:32:59 +0900

    ordersコントローラーアクション追加

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,2 +1,25 @@
 class Public::OrdersController < ApplicationController
+
+	def new
+	  @shipping_address = current_customer.shipping_address
+	  @shipping_address = ShippingAddress.new
+	end
+
+	def confirm
+	end
+
+	def create
+	end
+
+	def index
+	  @orders = Order.where(customer_id: current_customer.id)
+	end
+
+	def show
+	  @order = Order.find(params[:id])
+	end
+
+	def complete
+	end
+
 end
