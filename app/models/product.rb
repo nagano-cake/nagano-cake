d8bbc8e863ad075324be92b164174224e2bc10d5
Tomoyuki4630 <t.lagche4630@gmail.com>
2020-09-07 17:40:33 +0900

    products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,8 @@
 class Product < ApplicationRecord
 	# enum status: { 販売中: false, 販売中止: true}
 	attachment :image
+
 	has_many :carts_items
-	has_many :products
+	has_many :order_information
 	belongs_to :genre
 end
